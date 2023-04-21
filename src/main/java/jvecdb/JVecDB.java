01d45e68e14288f0a1f9dccd54a02de751992e46
gk646 <lukas.gilch@gmx.de>
2023-04-21 12:29:54 +0200

    ui updates

--- src/main/java/jvecdb/JVecDB.java
+++ src/main/java/jvecdb/JVecDB.java
@@ -19,11 +19,7 @@ import jvecdb.utils.enums.VectorShape;
 import jvecdb.utils.errorhandling.Alerts;
 import jvecdb.utils.errorhandling.exceptions.StartupFailure;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
